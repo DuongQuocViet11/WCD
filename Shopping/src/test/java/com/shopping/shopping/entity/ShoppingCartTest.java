9414fe966119adb5971e0164ea753fd116b2c680
DELL <duongquocv@gmail.com>
2022-05-19 11:25:59 +0700

    update

---
+++ Shopping/src/test/java/com/shopping/shopping/entity/ShoppingCartTest.java
@@ -0,0 +1,13 @@
+package com.shopping.shopping.entity;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShoppingCartTest {
+
+    @Test
+    void add() {
+
+    }
+}
